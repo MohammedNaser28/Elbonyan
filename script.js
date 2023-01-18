d0cd1a78c79df22eb1ca2d829bb2c3fefeca7bf3
Mohammed Naser <102864258+MohammedNaser28@users.noreply.github.com>
2023-01-19 00:55:08 +0200

    End Section

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-console.log("Hello world!");
+console.log("Hello World!");
 
-const myName = "Jonas Schmedtmann";
+const myName = "Mohammed Naser";
 const h1 = document.querySelector(".heading-primary");
 console.log(myName);
 console.log(h1);
 
-///////////////////////////////////////////////////////////
+// h1.textContent = myName
+
+// h1.style.backgroundColor = 'red';
+// h1.style.padding = "5rem"
+
+// h1.addEventListener('click',function(){
+//   h1.textContent = myName
+
+//   h1.style.backgroundColor = 'red';
+//   h1.style.padding = "5rem"
+// })
 // Set current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
@@ -14,69 +24,59 @@ yearEl.textContent = currentYear;
 ///////////////////////////////////////////////////////////
 // Make mobile navigation work
 
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".header");
+const navButton = document.querySelector(".btn-mobile-nav");
+const headerElement = document.querySelector(".header");
 
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
+navButton.addEventListener("click", function () {
+  headerElement.classList.toggle("nav-open");
 });
-
 ///////////////////////////////////////////////////////////
-// Smooth scrolling animation
+// smooth scrolling animation
 
-const allLinks = document.querySelectorAll("a:link");
-
-allLinks.forEach(function (link) {
+const id = document.querySelectorAll("a:link");
+id.forEach(function (link) {
   link.addEventListener("click", function (e) {
     e.preventDefault();
     const href = link.getAttribute("href");
-
-    // Scroll back to top
+    console.log(href);
+    //scroll back to top
     if (href === "#")
       window.scrollTo({
         top: 0,
         behavior: "smooth",
       });
-
-    // Scroll to other links
+    // SCroll to other linke
     if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
+      const SectionEl = document.querySelector(href);
+      SectionEl.scrollIntoView({ behavior: "smooth" });
     }
-
-    // Close mobile naviagtion
-    if (link.classList.contains("main-nav-link"))
+    // close nav
+    if (link.classList.contains("nav-header"))
       headerEl.classList.toggle("nav-open");
   });
 });
-
-///////////////////////////////////////////////////////////
-// Sticky navigation
-
-const sectionHeroEl = document.querySelector(".hero-section");
-
-const obs = new IntersectionObserver(
-  function (entries) {
-    const ent = entries[0];
-    console.log(ent);
-
-    if (ent.isIntersecting === false) {
-      document.body.classList.add("sticky");
-    }
-
-    if (ent.isIntersecting === true) {
-      document.body.classList.remove("sticky");
-    }
-  },
-  {
-    // In the viewport
-    root: null,
-    threshold: 0,
-    rootMargin: "-80px",
-  }
-);
-obs.observe(sectionHeroEl);
-
+//////////////////////////////////////////////////////////
+// Stickey nav
+// const sectionHero = document.querySelector(".hero-section");
+//
+// const stick = new IntersectionObserver(
+//   function (entries) {
+//     const en = entries[0];
+//     if (en.isIntersecting === false) {
+//       document.body.classList.add("sticky");
+//     }
+//     if (en.isIntersecting === true) {
+//       document.body.classList.remove("sticky");
+//     }
+//   },
+//   {
+//     // In the viewport
+//     // root: null,
+//     threshold: 0,
+//     rootMargin: "-80px",
+//   }
+// );
+// stick.observe(sectionHero);
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
